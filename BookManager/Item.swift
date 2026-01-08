7a1500f590519a31c4e3913b69428b9f748ac2b0
Wilbert Baker <Wilb1841@gmail.com>
2026-01-07 20:36:48 -0500

    Initial Commit

---
+++ BookManager/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BookManager
+//
+//  Created by Wilbert Baker on 1/7/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BookManagerTests/BookManagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  BookManagerTests.swift
+//  BookManagerTests
+//
+//  Created by Wilbert Baker on 1/7/26.
+//
+
+import Testing
+
+struct BookManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
